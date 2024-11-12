05df4db26c02e03da4487e47d35f3453a4e9570b
TheRaiymbekov <irajymbekov8@gmail.com>
2024-11-12 15:37:19 +0600

    lesson4

---
+++ lesson4/src/components/todo/Todo.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import classes from './Todo.module.scss';
+import Button from '../button/Button';
+
+
+const Todo = ({
+                  todo,
+                  handleDone,
+                  handleDelete,
+                  handleEdit,
+                  setCurrentId,
+                  isEdit
+              }) => {
+    const [ inputValue, setInputValue ] = useState(todo.title);
+    console.log(inputValue, 'inputValue');
+    return (
+        <>
+
+            <li className={`${classes.li} ${todo.completed && classes.done}`}>
+                <div className={classes.info}>
+                    <p>id: {todo.id}</p>
+                    <p>title: {todo.title}</p>
+                    <p>completed: {todo.completed ? 'true' : 'false'}</p>
+                </div>
+                <div className={classes.btns}>
+                    <Button name={'Edit'} action={() => setCurrentId(todo.id)} color={'primary'}/>
+                    <Button name={'Done'} action={() => handleDone(todo.id)} color={'secondary'}/>
+                    <Button name={'Delete'} action={() => {
+                        handleDelete(todo.id);
+                    }} color={'error'}/>
+                </div>
+            </li>
+            {
+                isEdit && <div className={classes.li}>
+                    <input
+                        type="text"
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}/>
+                    <Button name={'Save'} color={'primary'} action={() => handleEdit({
+                        ...todo,
+                        title: inputValue
+                    })}/>
+                    <Button name={'Cancel'} color={'error'} action={() => {
+                    }}/>
+                </div>
+            }
+
+        </>
+    );
+};
+
+export default Todo;
