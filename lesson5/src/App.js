4c3ac3305a567e12a4145e1672ddbf3652e7ba3f
TheRaiymbekov <irajymbekov8@gmail.com>
2024-11-19 20:14:36 +0600

    lesson6

---
+++ lesson5/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MainPage from "./pages/MainPage";
+import TodoPage from "./pages/TodoPage";
+import React from "react";
+
+function App() {
+    return (
+        <div className="App">
+            {/*<MainPage/>*/}
+            <TodoPage/>
+        </div>
+    );
+}
+
+export default App;
